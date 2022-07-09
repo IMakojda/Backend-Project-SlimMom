3d7a9013eb1e7268da7b7c8740caff56a1986240
Oleksandr Khvaleba <alex8inbox@gmail.com>
2022-07-09 19:59:22 +0300

    validation fix

--- models/calc.js
+++ models/calc.js
@@ -1,10 +1,10 @@
 const Joi = require('joi');
 
 const schemaCalc = Joi.object({
-  height: Joi.number().integer().required(),
-  age: Joi.number().integer().required(),
-  currentWeight: Joi.number().required(),
-  desiredWeight: Joi.number().required(),
+  height: Joi.number().min(100).max(250).integer().required(),
+  age: Joi.number().min(18).max(100).integer().required(),
+  currentWeight: Joi.number().min(20).max(500).required(),
+  desiredWeight: Joi.number().min(20).max(500).required(),
   bloodType: Joi.number().integer().min(1).max(4).required(),
 });
 
